16f690369a3543bfbb4cf371da16caa404df8525
Hrafn Eiríksson <hrafne@gmail.com>
2013-11-17 01:00:27 +0000

    Were getting here.

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-var crawler = require('./lib/crawler.js');
+var Crawler = require('./lib/crawler.js');
+
+var crawler = new Crawler({
+	max_pages: 1
+});
+
+crawler.start('http://www.mbl.is', 'golf', 'birgir leifur');
